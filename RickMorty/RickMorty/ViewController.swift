2a735cae71c31bd2101bd8e5268b29e979933e9d
Paola Golombieski Ciarcia <paolaciarcia20@gmail.com>
2023-02-10 17:29:48 -0300

    Add Xode project and test dependencies in podfile

---
+++ RickMorty/RickMorty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickMorty
+//
+//  Created by Paola Golombieski Ciarcia on 10/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
